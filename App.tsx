b7132e55122bd62809666c07f074e3fd3451c475
Shivam Kumar Nayak <snayak@datopic.com>
2025-02-13 18:42:30 +0530

    Implement foreground and background message handling

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@ import { useEffect, useState } from 'react';
 
 export default function App() {
   const [token, setToken] = useState<string>("");
+  const [initialNotification, setInitialNotification] = useState<string>("");
+  const [openAppNotification, setOpenAppNotification] = useState<string>("");
+  const [backgroundNotification, setBackgroundNotification] = useState<string>("");
   const requestUserPermission = async (): Promise<boolean> => {
     const authStatus = await messaging().requestPermission();
     const enabled =
@@ -30,7 +33,26 @@ export default function App() {
         console.log('########## Permission not granted');
       }
 
-      
+      // Check whether an initial notification is available 
+      messaging().getInitialNotification().then(async (remoteMessage) =>{
+        if(remoteMessage){
+          console.log("Notification caused app to open from quit state: ", remoteMessage.notification);
+          setInitialNotification(JSON.stringify(remoteMessage));
+        }
+      })
+
+      // Assume a message-notification contains a "type" property in the data payload of the screen to open. 
+
+      messaging().onNotificationOpenedApp(async(remoteMessage) =>{
+        console.log("Notification cause app to open from background state: ", remoteMessage.notification);
+        setOpenAppNotification(JSON.stringify(remoteMessage));
+      })
+
+      // Register background handler
+      messaging().setBackgroundMessageHandler( async(remoteMessage) =>{
+        console.log("Message handled in the background! ", remoteMessage.notification);
+        setBackgroundNotification(JSON.stringify(remoteMessage));
+      })
     };
 
     fetchToken();
@@ -44,6 +66,18 @@ export default function App() {
       <Text style={{ marginTop: 10, fontWeight: 'bold' }}>Token:</Text>
       <Text style={{ padding: 10, backgroundColor: '#f4f4f4', borderRadius: 5 }}>
         {token || "Fetching token..."}
+      </Text>
+      <Text style={{ marginTop: 10, fontWeight: 'bold' }}>initialNotification:</Text>
+      <Text style={{ padding: 10, backgroundColor: '#f4f4f4', borderRadius: 5 }}>
+        {initialNotification || "Fetching initialNotification..."}
+      </Text>
+      <Text style={{ marginTop: 10, fontWeight: 'bold' }}>openAppNotification:</Text>
+      <Text style={{ padding: 10, backgroundColor: '#f4f4f4', borderRadius: 5 }}>
+        {openAppNotification || "Fetching openAppNotification..."}
+      </Text>
+      <Text style={{ marginTop: 10, fontWeight: 'bold' }}>backgroundNotification:</Text>
+      <Text style={{ padding: 10, backgroundColor: '#f4f4f4', borderRadius: 5 }}>
+        {backgroundNotification || "Fetching backgroundNotification..."}
       </Text>
       <StatusBar style="auto" />
     </View>
